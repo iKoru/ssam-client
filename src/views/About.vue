95af16b48d67c36cdba54fb2f803cc47ef2340ef
gon <ngongon.kim@gmail.com>
2018-10-21 13:52:21 +0900

    vue-quill-editor forked and testing

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,55 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <!-- bidirectional data binding（双向数据绑定） -->
+  <div>
+    <quill-editor v-model="content"
+                  ref="myQuillEditor"
+                  :options="editorOption"
+                  @blur="onEditorBlur($event)"
+                  @focus="onEditorFocus($event)"
+                  @ready="onEditorReady($event)">
+    </quill-editor>
   </div>
 </template>
+
+<script>
+// you can also register quill modules in the component
+// import Quill from 'quill'
+// import { someModule } from '../yourModulePath/someQuillModule.js'
+// Quill.register('modules/someModule', someModule)
+
+export default {
+  data () {
+    return {
+      content: '<h2>I am Example</h2>',
+      editorOption: {
+        // some quill options
+      }
+    }
+  },
+  // manually control the data synchronization
+  // 如果需要手动控制数据同步，父组件需要显式地处理changed事件
+  methods: {
+    onEditorBlur (quill) {
+      console.log('editor blur!', quill)
+    },
+    onEditorFocus (quill) {
+      console.log('editor focus!', quill)
+    },
+    onEditorReady (quill) {
+      console.log('editor ready!', quill)
+    },
+    onEditorChange ({ quill, html, text }) {
+      console.log('editor change!', quill, html, text)
+      this.content = html
+    }
+  },
+  computed: {
+    editor () {
+      return this.$refs.myQuillEditor.quill
+    }
+  },
+  mounted () {
+    console.log('this is current quill instance object', this.editor)
+  }
+}
+</script>
