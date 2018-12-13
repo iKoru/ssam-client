dc4311a6fd7a12387de3f2caaa665955434aaf37
koru <glassonyou@gmail.com>
2018-12-13 09:01:12 +0000

    adjust environment to c9

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,18 @@ var webpack = require('webpack')
 
 module.exports = {
   outputDir: '../client',
+  
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
         'window.Quill': 'quill/dist/quill.js', // image-resize-module requires window.Quill
         'Quill': 'quill/dist/quill.js'
       })
-    ]
+    ],
+    devServer:{
+      compress:true,
+      disableHostCheck:true,
+      port:8082
+    }
   }
 }
