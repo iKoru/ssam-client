b6820a04fc7e603a1ee2295e08d84489b1f6227e
iKoru <glassonyou@gmail.com>
2018-12-14 22:49:35 +0900

    apply dynamic component layout - in progress(1)

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,66 @@
 <template>
   <v-app>
+    <component :is="layout">
+      <router-view :layout.sync="layout"/>
+    </component>
     <v-dialog v-model="spinner" width="300">
-      <v-card color="indigo" dark>
+      <v-card color="indigo" :dark="$store.getters.isLight">
         <v-card-text>
           <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
         </v-card-text>
       </v-card>
     </v-dialog>
-    <router-view exact/>
   </v-app>
 </template>
 
 <script>
-import { eventHub } from './plugins/eventhub'
+import eventHub from "./plugins/eventhub";
 
 export default {
-  name: 'App',
+  name: "App",
   data: () => ({
-    spinner: false
+    spinner: false,
+    layout: "div"
   }),
   methods: {
-    showSpinner () {
-      console.log('spinner on')
-      this.spinner = true
+    showSpinner() {
+      console.log("spinner on");
+      this.spinner = true;
     },
-    hideSpinner () {
-      console.log('spinner off')
-      this.spinner = false
+    hideSpinner() {
+      console.log("spinner off");
+      this.spinner = false;
     },
-    refresh (error) {
-      console.log(error)
+    refresh(error) {
+      console.log(error);
     }
   },
-  created () {
-    eventHub.$on('before-request', this.showSpinner)
-    eventHub.$on('request-error', this.hideSpinner)
-    eventHub.$on('after-response', this.hideSpinner)
-    eventHub.$on('response-error', this.hideSpinner)
-    eventHub.$on('response-error', this.refresh)
+  created() {
+    eventHub.$on("before-request", this.showSpinner);
+    eventHub.$on("request-error", this.hideSpinner);
+    eventHub.$on("after-response", this.hideSpinner);
+    eventHub.$on("response-error", this.hideSpinner);
+    eventHub.$on("response-error", this.refresh);
   },
-  beforeDestroy () {
-    eventHub.$off('before-request', this.showSpinner)
-    eventHub.$off('request-error', this.hideSpinner)
-    eventHub.$off('after-response', this.hideSpinner)
-    eventHub.$off('response-error', this.hideSpinner)
-  },
-  middleware: 'requireAuth'
-}
+  beforeDestroy() {
+    eventHub.$off("before-request", this.showSpinner);
+    eventHub.$off("request-error", this.hideSpinner);
+    eventHub.$off("after-response", this.hideSpinner);
+    eventHub.$off("response-error", this.hideSpinner);
+  }
+};
 </script>
 
 <style>
 @import url(http://fonts.googleapis.com/earlyaccess/nanumgothic.css);
 
 .w-100 {
-  width: 100%
+  width: 100%;
 }
-.float-right{
+.float-right {
   float: right;
 }
 body {
-  font-family: 'Nanum Gothic'!important;
+  font-family: "Nanum Gothic" !important;
 }
 </style>
