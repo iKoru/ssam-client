0f33051810589a16cb5e566484e57a6182c536ff
iKoru <glassonyou@gmail.com>
2018-12-17 23:19:47 +0900

    centered signin component

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,7 +1,18 @@
 <template>
-  <v-container>
-    <v-layout>
-      <Signin/>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center app>
+      <v-flex xs12 sm8 md6 lg4>
+        <v-content>
+          <v-card flat>
+            <v-card-title>
+              <div xs12>
+                <span class="headline">pedagy 로그인</span>
+              </div>
+            </v-card-title>
+            <Signin/>
+          </v-card>
+        </v-content>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -14,8 +25,16 @@ export default {
   },
   created() {
     this.$emit("update:layout", PublicLayout);
-  }
+  },
+  name: "SigninPage"
 };
 </script>
-<style scoped>
+<style>
+.publicLayout {
+  background-color: white;
+  height: 100%;
+}
+.publicLayout__main {
+  height: 100%;
+}
 </style>
