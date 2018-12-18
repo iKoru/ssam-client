6a45e06074bcefe50bfeb5e8a9b837478ed5635b
koru <glassonyou@gmail.com>
2018-12-18 06:12:08 +0000

    remove footer in index layout

--- src/layouts/IndexLayout.vue
+++ src/layouts/IndexLayout.vue
@@ -3,16 +3,10 @@
     <main class="indexLayout__main">
       <slot></slot>
     </main>
-    <footer class="indexLayout__footer">
-      <main-footer></main-footer>
-    </footer>
   </div>
 </template>
 <script>
-import MainFooter from "../components/MainFooter";
 export default {
-  components: {
-    MainFooter
-  }
+  
 };
 </script>
