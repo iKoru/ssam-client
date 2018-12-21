6b55bedc0d1244ddc5f0a415a3ded3c4e802af4b
koru <glassonyou@gmail.com>
2018-12-21 06:13:27 +0000

    implemented boardExtractor

--- src/components/board/BoardExtractor.vue
+++ src/components/board/BoardExtractor.vue
@@ -1,64 +1,68 @@
 
 <template>
-<div>
-  <template v-for="(item, index) in items">
-    <v-subheader
-      v-if="item.header"
-      :key="item.header"
-    >
-      {{ item.header }}
-    </v-subheader>
-
-    <v-divider
-      v-else-if="item.divider"
-      :inset="item.inset"
-      :key="index"
-    ></v-divider>
-
-    <v-list-tile
-      v-else
-      :key="item.title"
-      avatar
-    >
-      <v-list-tile-avatar>
-        <img :src="item.avatar">
-      </v-list-tile-avatar>
-
-      <v-list-tile-content>
-        <v-list-tile-title v-html="item.title"></v-list-tile-title>
-        <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-      </v-list-tile-content>
-    </v-list-tile>
-  </template>
+  <div>
+    <span>기간별 베스트</span>
+    <v-flex xs12 class="py-2 mx-2">
+      <v-btn-toggle v-model="period" id="boardExtractorBtnGroup" flat>
+        <v-btn flat value="daily">
+          오늘
+        </v-btn>
+        <v-btn flat value="weekly">
+          이번주
+        </v-btn>
+        <v-btn flat value="monthly">
+          이번달
+        </v-btn>
+      </v-btn-toggle>
+    </v-flex>
+    <small-document-list :list="targets"></small-document-list>
+    
   </div>
 </template>
 <script>
-
+import SmallDocumentList from './SmallDocumentList'
 export default {
-  name: 'BoardExtractor',
+  name: 'BoardExtractor', 
+  components:{
+    SmallDocumentList
+  },
   data () {
     return {
-      items: [
-        { header: 'Today' },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'Brunch this weekend?',
-          subtitle: "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle: "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
-        }
-      ]
+      targets: [],
+      period: 'daily',
+      items: null
+    }
+  },
+  created(){
+    this.$axios.get('/best', {params:{boardType:'L'}})
+    .then(response => {
+      this.items = response.data;
+      this.targets = this.items.daily
+    })
+    .catch(error => {
+      console.log(error);
+      this.$store.dispatch('showSnackbar', {text:`베스트 게시물을 가져오는 데 오류가 발생했습니다.${error.response?'['+error.response.data.message+']': ''}`, color:'error'})
+    })
+  },
+  watch:{
+    period(val){
+      switch(val){
+        case 'daily':
+          this.targets = this.items.daily
+          break;
+        case 'weekly':
+          this.targets = this.items.weekly
+          break;
+        case 'monthly':
+          this.targets = this.items.monthly
+          break;
+      }
     }
   }
 }
 </script>
+<style>
+  #boardExtractorBtnGroup{
+    width:100%;
+  }
+</style>
