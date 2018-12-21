5aad0bbbd29374c4481d099075696717125ca4fd
iKoru <glassonyou@gmail.com>
2018-12-21 22:31:10 +0900

    change list key to documentId

--- src/components/board/SmallDocumentList.vue
+++ src/components/board/SmallDocumentList.vue
@@ -1,39 +1,40 @@
 <template>
-    <div>
-        <template v-if="list.length === 0">
-            <div>
-                표시할 내용이 없습니다.
-            </div>
-        </template>
-        <v-list v-else dense>
-            <template v-for="(item, index) in list">
-              <v-subheader v-if="item.header" :key="item.header">
-                {{ item.header }}
-              </v-subheader>
-            
-              <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
-            
-              <v-list-tile v-else :key="item.title" :to="'/'+item.boardId+'/'+item.documentId" ripple>
-                <v-list-tile-content>
-                  <v-list-tile-title>
-                      <v-layout row>
-                          <span class="ellipsis">{{item.title}}</span><v-spacer/><span class="tiny align-center"><v-icon color="red darken-2">favorite</v-icon> {{item.voteUpCount}}</span>
-                      </v-layout>
-                  </v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </template>
-        </v-list>
-    </div>
+  <div>
+    <template v-if="list.length === 0">
+      <div>표시할 내용이 없습니다.</div>
+    </template>
+    <v-list v-else dense>
+      <template v-for="(item, index) in list">
+        <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+
+        <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+
+        <v-list-tile v-else :key="item.documentId" :to="'/'+item.boardId+'/'+item.documentId" ripple>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              <v-layout row>
+                <span class="ellipsis">{{item.title}}</span>
+                <v-spacer/>
+                <span class="tiny align-center">
+                  <v-icon color="red darken-2">favorite</v-icon>
+                  {{item.voteUpCount}}
+                </span>
+              </v-layout>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </template>
+    </v-list>
+  </div>
 </template>
 <script>
 export default {
-    name:'smallDocumentList',
-    props:['list']
-}
+  name: "smallDocumentList",
+  props: ["list"]
+};
 </script>
 <style>
-.ellipsis{
-    max-width:275px;
+.ellipsis {
+  max-width: 275px;
 }
 </style>
