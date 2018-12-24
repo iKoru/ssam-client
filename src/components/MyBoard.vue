7e5a17e2aa7aa0143ae0af72d1d57c16e89f5c66
iKoru <glassonyou@gmail.com>
2018-12-24 20:29:56 +0900

    implementing logic for creating topic

--- src/components/MyBoard.vue
+++ src/components/MyBoard.vue
@@ -5,14 +5,19 @@
         <v-layout row wrap>
           <v-flex xs12 :sm6="lounges.length > 0" :sm9="lounges.length === 0" :lg5="lounges.length > 0" :lg8="lounges.length === 0" :class="{'pr-2':$vuetify.breakpoint.mdAndUp, 'ml-auto':true, 'mr-auto':lounges.length === 0}">
             <v-layout row class="titleRow">
-              <span class="title d-inline-block">구독중인 토픽</span><v-spacer/><v-btn id="createTopic" small @click="openDialog" color="accent" :class="{'my-0':true, 'ml-0':true, 'mr-0':$vuetify.breakpoint.xsOnly}" title="새로운 토픽을 만들 수 있습니다.">토픽만들기</v-btn><br/>
+              <span class="title d-inline-block">구독중인 토픽</span>
+              <v-spacer/>
+              <v-btn id="createTopic" small @click="openDialog" color="accent" :class="{'my-0':true, 'ml-0':true, 'mr-0':$vuetify.breakpoint.xsOnly}" title="새로운 토픽을 만들 수 있습니다.">토픽만들기</v-btn>
+              <br>
             </v-layout>
             <span class="ellipsis subtitle">드래그하여 순서를 변경할 수 있습니다.</span>
             <v-divider class="my-3"/>
             <draggable v-model="topics" v-if="topics.length > 0">
               <transition-group>
                 <v-layout class="cursor-move" row v-for="(topic, index) in topics" :key="topic.boardId">
-                  <v-flex class="ellipsis"><div class="vertical-center ellipsis">{{topic.boardName}}</div></v-flex>
+                  <v-flex class="ellipsis">
+                    <div class="vertical-center ellipsis">{{topic.boardName}}</div>
+                  </v-flex>
                   <v-spacer/>
                   <v-btn v-if="!topic.readRestrictDate && !topic.writeRestrictDate" flat primary small class="mx-0" @click="removeItem(index)">구독해제</v-btn>
                   <template v-else>
@@ -28,8 +33,8 @@
                 </v-layout>
               </transition-group>
             </draggable>
-            <v-layout v-else>
-              아직 구독하고 있는 토픽이 없네요.<br/>새로운 토픽을 구독해보세요!
+            <v-layout v-else>아직 구독하고 있는 토픽이 없네요.
+              <br>새로운 토픽을 구독해보세요!
             </v-layout>
           </v-flex>
           <v-flex xs12 sm6 lg5 v-if="lounges.length > 0" :class="{'mt-5':$vuetify.breakpoint.xsOnly, 'pl-2':$vuetify.breakpoint.mdAndUp, 'mr-auto':true}">
@@ -41,7 +46,9 @@
             <v-list dense>
               <v-list-tile v-for="lounge in lounges" :key="lounge.boardId">
                 <v-layout row>
-                  <v-flex class="ellipsis"><div class="vertical-center ellipsis">{{lounge.boardName}}</div></v-flex>
+                  <v-flex class="ellipsis">
+                    <div class="vertical-center ellipsis">{{lounge.boardName}}</div>
+                  </v-flex>
                   <v-spacer/>
                   <v-tooltip v-if="lounge.writeRestrictDate" bottom>
                     <v-chip slot="activator" color="red" text-color="white" small>쓰기제한</v-chip>
@@ -58,7 +65,7 @@
         </v-layout>
       </v-layout>
       <v-dialog v-model="dialog" :fullscreen="$vuetify.breakpoint.xsOnly" :transition="$vuetify.breakpoint.xsOnly?'dialog-bottom-transition':'fade-transition'" scrollable lazy max-width="700px">
-        <topic-creator @closeDialog="closeDialog"/>
+        <topic-creator @closeDialog="closeDialog" @resetBoard="resetBoard"/>
       </v-dialog>
     </v-card-title>
     <v-card-actions pa-3>
@@ -72,7 +79,7 @@
 </template>
 <script>
 import draggable from "vuedraggable";
-import TopicCreator from './TopicCreator';
+import TopicCreator from "./TopicCreator";
 export default {
   name: "MyBoard",
   components: {
@@ -83,22 +90,22 @@ export default {
     return {
       loading: false,
       dialog: false,
-      topics: [{boardId: 1, boardName: "aadddddddddddddddda", readRestrictDate:'20181231'}, {boardId: 2, boardName: "afffffffffffffffdaa", writeRestrictDate:'20191111'}, {boardId: 3, boardName: "afaggggggggggggggga", readRestrictDate:'20180101', writeRestrictDate:'20180301'}, {boardId: 4, boardName: "aasaa"}, {boardId: 5, boardName: "agdgaa"}, {boardId: 6, boardName: "aagga"}, {boardId: 7, boardName: "azzzaa"}, {boardId: 8, boardName: "aeeeaa"}, {boardId: 9, boardName: "azzzzzzaab"}],
-      lounges: [{boardId: 1, boardName: "addddddddddddddddddddddaa", readRestrictDate:'20181231'}, {boardId: 2, boardName: "adddddddddddddddddddaa", writeRestrictDate:'20191111'}, {boardId: 3, boardName: "afaaaaaaaaaaaaaaaa"}, {boardId: 4, boardName: "aasaa", readRestrictDate:'20180101', writeRestrictDate:'20180301'}, {boardId: 5, boardName: "agdgaa"}, {boardId: 6, boardName: "aagga"}, {boardId: 7, boardName: "azzzaa"}, {boardId: 8, boardName: "aeeeaa"}, {boardId: 9, boardName: "azzzzzzaab"}],
-      originalTopics:[{boardId: 1, boardName: "aaa"}, {boardId: 2, boardName: "adaa"}, {boardId: 3, boardName: "afaa"}, {boardId: 4, boardName: "aasaa"}, {boardId: 5, boardName: "agdgaa"}, {boardId: 6, boardName: "aagga"}, {boardId: 7, boardName: "azzzaa"}, {boardId: 8, boardName: "aeeeaa"}, {boardId: 9, boardName: "azzzzzzaab"}]
+      topics: [{boardId: 1, boardName: "aadddddddddddddddda", readRestrictDate: "20181231"}, {boardId: 2, boardName: "afffffffffffffffdaa", writeRestrictDate: "20191111"}, {boardId: 3, boardName: "afaggggggggggggggga", readRestrictDate: "20180101", writeRestrictDate: "20180301"}, {boardId: 4, boardName: "aasaa"}, {boardId: 5, boardName: "agdgaa"}, {boardId: 6, boardName: "aagga"}, {boardId: 7, boardName: "azzzaa"}, {boardId: 8, boardName: "aeeeaa"}, {boardId: 9, boardName: "azzzzzzaab"}],
+      lounges: [{boardId: 1, boardName: "addddddddddddddddddddddaa", readRestrictDate: "20181231"}, {boardId: 2, boardName: "adddddddddddddddddddaa", writeRestrictDate: "20191111"}, {boardId: 3, boardName: "afaaaaaaaaaaaaaaaa"}, {boardId: 4, boardName: "aasaa", readRestrictDate: "20180101", writeRestrictDate: "20180301"}, {boardId: 5, boardName: "agdgaa"}, {boardId: 6, boardName: "aagga"}, {boardId: 7, boardName: "azzzaa"}, {boardId: 8, boardName: "aeeeaa"}, {boardId: 9, boardName: "azzzzzzaab"}],
+      originalTopics: [{boardId: 1, boardName: "aaa"}, {boardId: 2, boardName: "adaa"}, {boardId: 3, boardName: "afaa"}, {boardId: 4, boardName: "aasaa"}, {boardId: 5, boardName: "agdgaa"}, {boardId: 6, boardName: "aagga"}, {boardId: 7, boardName: "azzzaa"}, {boardId: 8, boardName: "aeeeaa"}, {boardId: 9, boardName: "azzzzzzaab"}]
     };
   },
   methods: {
     reset() {
       this.topics = JSON.parse(JSON.stringify(this.originalTopics));
     },
-    isDirty(){
-      if(this.topics.length !== this.originalTopics.length){
+    isDirty() {
+      if (this.topics.length !== this.originalTopics.length) {
         return true;
       }
-      let i=0;
-      while(i<this.topics.length){
-        if(this.topics[i].boardId !== this.originalTopics[i].boardId){
+      let i = 0;
+      while (i < this.topics.length) {
+        if (this.topics[i].boardId !== this.originalTopics[i].boardId) {
           return true;
         }
         i++;
@@ -106,52 +113,67 @@ export default {
       return false;
     },
     save() {
-      if(!this.isDirty()){
-        this.$store.dispatch('showSnackbar', {text:'변경된 내용이 없습니다.', color:'info'})
+      if (!this.isDirty()) {
+        this.$store.dispatch("showSnackbar", {text: "변경된 내용이 없습니다.", color: "info"});
         return;
       }
-      const param = this.topics.map((item, index) => ({orderNumber:index+1, boardId:item.boardId}))
       this.loading = true;
-      this.$axios.put('/user/board', {boards:param})
-      .then(responose => {
-        this.loading = false;
-        this.originalTopics = param;
-        this.reset();
-        this.$store.dispatch('showSnackbar', {text:'변경 내용을 반영하였습니다.', color:'success'});
-      })
-      .catch(error => {
-        console.log(error);
-        this.loading = false;
-        this.$store.dispatch('showSnackbar', {text:error.response?error.response.data.message || '변경 내용을 반영하지 못했습니다.' : '변경내용을 반영하지 못했습니다.', color:'error'})
-      })
+      this.$axios
+        .put("/user/board", {boards: this.topics.map((item, index) => ({orderNumber: index + 1, boardId: item.boardId}))})
+        .then(responose => {
+          this.loading = false;
+          this.originalTopics = JSON.parse(JSON.stringify(this.topics));
+          this.reset();
+          this.$store.dispatch("showSnackbar", {text: "변경 내용을 반영하였습니다.", color: "success"});
+        })
+        .catch(error => {
+          console.log(error);
+          this.loading = false;
+          this.$store.dispatch("showSnackbar", {text: error.response ? error.response.data.message || "변경 내용을 반영하지 못했습니다." : "변경내용을 반영하지 못했습니다.", color: "error"});
+        });
     },
-    removeItem(index){
+    removeItem(index) {
       this.topics.splice(index, 1);
     },
-    openDialog(){
-      if(this.$store.getters.profile.status !== 'AUTHORIZED'){
-        this.$store.dispatch('showSnackbar', {text:'인증을 받은 회원만 토픽을 만들 수 있습니다.', color:'error'});
+    openDialog() {
+      if (this.$store.getters.profile.status !== "AUTHORIZED") {
+        this.$store.dispatch("showSnackbar", {text: "인증을 받은 회원만 토픽을 만들 수 있습니다.", color: "error"});
         return;
       }
       this.dialog = true;
     },
-    closeDialog(){
+    closeDialog() {
       this.dialog = false;
+    },
+    resetBoard() {
+      this.$axios
+        .get("/user/board")
+        .then(response => {
+          console.log(response);
+          this.originalTopics = response.data.filter(x => x.boardType === "T");
+          this.lounges = response.data.filter(x => x.boardType !== "T");
+          this.reset();
+        })
+        .catch(error => {
+          console.log(error);
+          this.$store.dispatch("showSnackbar", {text: error.response ? error.response.data.message || "구독 게시판 목록을 불러오지 못했습니다." : "구독 게시판 목록을 불러오지 못했습니다.", color: "error"});
+        });
     }
   },
-  mounted(){
-    console.log('load topics and lounges...')
-    this.$axios.get('/user/board')
-    .then(response => {
-      console.log(response);
-      this.originalTopics = response.data.filter(x=>x.boardType === 'T');
-      this.lounges = response.data.filter(x=>x.boardType !== 'T');
-      this.reset();
-    })
-    .catch(error => {
-      console.log(error);
-      this.$store.dispatch('showSnackbar', {text:error.response?error.response.data.message || '구독 게시판 목록을 불러오지 못했습니다.' : '구독 게시판 목록을 불러오지 못했습니다.', color:'error'})
-    });
+  mounted() {
+    console.log("load topics and lounges...");
+    this.$axios
+      .get("/user/board")
+      .then(response => {
+        console.log(response);
+        this.originalTopics = response.data.filter(x => x.boardType === "T");
+        this.lounges = response.data.filter(x => x.boardType !== "T");
+        this.reset();
+      })
+      .catch(error => {
+        console.log(error);
+        this.$store.dispatch("showSnackbar", {text: error.response ? error.response.data.message || "구독 게시판 목록을 불러오지 못했습니다." : "구독 게시판 목록을 불러오지 못했습니다.", color: "error"});
+      });
   },
   render(h) {
     return h("myBoard", {attrs: {id: "app"}}, this.draggable);
@@ -159,20 +181,20 @@ export default {
 };
 </script>
 <style>
-  .cursor-move{
-    height:40px;
-  }
-  .subtitle{
-    display:block;
-  }
-  .v-tooltip .v-chip{
-    margin-top:8px;
-    margin-bottom:8px;
-  }
-  .titleRow{
-    height:24px;
-  }
-  #createTopic{
-    top:-4px;
-  }
+.cursor-move {
+  height: 40px;
+}
+.subtitle {
+  display: block;
+}
+.v-tooltip .v-chip {
+  margin-top: 8px;
+  margin-bottom: 8px;
+}
+.titleRow {
+  height: 24px;
+}
+#createTopic {
+  top: -4px;
+}
 </style>
--- src/components/TopicCreator.vue
+++ src/components/TopicCreator.vue
@@ -11,7 +11,7 @@
       </v-toolbar-items>
     </v-toolbar>
     <v-card-text>
-      <v-form>
+      <v-form ref="form">
         <v-layout row xs12 wrap>
           <v-flex xs6 class="mb-1">
             <v-text-field v-model="boardId" :rules="boardIdRules" class="dense" :error-messages="boardIdErrors" maxlength="15" label="토픽ID" required hint="주소창에 들어갈 토픽의 ID입니다." validate-on-blur @blur="checkBoardId"></v-text-field>
@@ -40,9 +40,14 @@
             </v-layout>
           </v-flex>
           <v-flex xs12>
-            <v-autocomplete name="allowedGroups" chips multiple item-text="text" item-value="value" v-model="allowedGroups" :disabled="allGroupAuth === 'READWRITE'" :items="groupItems" label="구독허용 그룹" hint="(최소) 내가 구독할 수 있도록 선택해야 합니다." persistent-hint>
+            <v-autocomplete name="allowedGroups" chips multiple item-text="text" dense item-value="value" v-model="allowedGroups" :disabled="allGroupAuth === 'READWRITE'" :items="groupItems" label="구독허용 그룹" hint="(최소) 내가 구독할 수 있도록 선택해야 합니다." persistent-hint>
               <template slot="selection" slot-scope="props">
-                <v-chip close :key="props.item.value" :selected="props.selected" @input="removeChip(props, props.item, allowedGroups)">{{props.item.text}}</v-chip>
+                <v-chip close small :key="props.item.value" :selected="props.selected" @input="removeChip(props, props.item, allowedGroups)">{{props.item.text}}</v-chip>
+              </template>
+              <template slot="item" slot-scope="data">
+                <v-list-tile-content>
+                  <v-list-tile-title v-html="data.item.text"></v-list-tile-title>
+                </v-list-tile-content>
               </template>
             </v-autocomplete>
           </v-flex>
@@ -54,55 +59,46 @@
   </v-card>
 </template>
 <script>
-const reserved = ['document', 'documents', 'profile',
-    'profiles', 'auth', 'user', 'users', 'comment', 'comments',
-    'vote', 'votes', 'report', 'reports', 'index', 'scraps',
-    'scrap', 'board', 'boards', 'manage', 'manages', 'chat',
-    'chats', 'message', 'messages', 'group', 'groups', 'event',
-    'events', 'signup', 'signin', 'signout', 'resetPassword',
-    'notification', 'notifications', 'survey', 'list', 'admin', 'ADMIN',
-    'ADMINISTRATOR', 'administrator', 'attach', 'profiles', 'animal', 'loungeBest', 'topicBest',
-    'lounge', 'topic', 'type', 'best', 'sanction', 'userId', 'nickName',
-    'myPage', 'myBoard', 'myCommunity', 'pedagy', 'myPedagy', 'Pedagy'
-];
-const boardIdRegex = [/^(?:[a-zA-Z]+)(?:[a-zA-Z0-9\-_]{0,15})$/, /^((?!(--|__)).)*$/]
+const reserved = ["document", "documents", "profile", "profiles", "auth", "user", "users", "comment", "comments", "vote", "votes", "report", "reports", "index", "scraps", "scrap", "board", "boards", "manage", "manages", "chat", "chats", "message", "messages", "group", "groups", "event", "events", "signup", "signin", "signout", "resetPassword", "notification", "notifications", "survey", "list", "admin", "ADMIN", "ADMINISTRATOR", "administrator", "attach", "profiles", "animal", "loungeBest", "topicBest", "lounge", "topic", "type", "best", "sanction", "userId", "nickName", "myPage", "myBoard", "myCommunity", "pedagy", "myPedagy", "Pedagy"];
+const boardIdRegex = [/^(?:[a-zA-Z]+)(?:[a-zA-Z0-9\-_]{0,15})$/, /^((?!(--|__)).)*$/];
+const groupName = {M: "전공", G: "학년", N: "일반", R: "지역"};
 export default {
-  name:'topicCreator',
-  data(){
+  name: "topicCreator",
+  data() {
     return {
       loading: false,
-      boardId:null,
-      boardName:null,
-      boardDescription:null,
-      allGroupAuth:'READONLY',
-      allowAnonymous:true,
+      boardId: null,
+      boardName: null,
+      boardDescription: null,
+      allGroupAuth: "READONLY",
+      allowAnonymous: true,
       useCategory: false,
       allowedGroups: [],
-      allGroupAuthItems:[{value:'READWRITE', text:'전체구독허용'}, {value:'READONLY', text:'읽기공개'}, {value:'NONE', text:'비공개'}],
+      allGroupAuthItems: [{value: "READWRITE", text: "전체구독허용"}, {value: "READONLY", text: "읽기공개"}, {value: "NONE", text: "비공개"}],
       groupItems: [],
-      boardIdRules:[v=>!!v || '토픽ID를 입력해주세요.', v=>!reserved.includes(v) || '사용할 수 없는 ID입니다.', v=> (v&&v.length > 3&&v.length<16) || '4~15자로 입력해주세요.', v=> boardIdRegex[0].test(v) || '토픽ID의 길이가 너무 길거나, [_, -] 이외의 특수문자가 있습니다.', v=>boardIdRegex[1].test(v) || '토픽ID에 연속된 [_, -]가 있습니다.'],
-      boardNameRules:[v=>!!v || '토픽 이름을 입력해주세요.'],
-      boardIdErrors : [],
-    }
+      boardIdRules: [v => !!v || "토픽ID를 입력해주세요.", v => !reserved.includes(v) || "사용할 수 없는 ID입니다.", v => (v && v.length > 3 && v.length < 16) || "4~15자로 입력해주세요.", v => boardIdRegex[0].test(v) || "토픽ID의 길이가 너무 길거나, [_, -] 이외의 특수문자가 있습니다.", v => boardIdRegex[1].test(v) || "토픽ID에 연속된 [_, -]가 있습니다."],
+      boardNameRules: [v => !!v || "토픽 이름을 입력해주세요."],
+      boardIdErrors: []
+    };
   },
-  computed:{
-    allGroupAuthDescription(){
-      switch(this.allGroupAuth){
-        case 'READONLY':
-          return '구독자만 글을 쓸 수 있지만,<br>구독하지 않아도 글을 볼 수 있습니다.<br>그 대신 핫토픽에 글이 노출됩니다.'
-        case 'NONE':
-          return '구독자만 글을 읽고 쓸 수 있습니다.<br>핫토픽에는 글이 노출되지 않습니다.'
-        case 'READWRITE':
-          return '모든 회원이 토픽을 구독할 수 있습니다.<br>구독하지 않아도 글을 볼 수 있습니다.<br>핫토픽에 글이 노출됩니다.';
+  computed: {
+    allGroupAuthDescription() {
+      switch (this.allGroupAuth) {
+        case "READONLY":
+          return "구독자만 글을 쓸 수 있지만,<br>구독하지 않아도 글을 볼 수 있습니다.<br>그 대신 핫토픽에 글이 노출됩니다.";
+        case "NONE":
+          return "구독자만 글을 읽고 쓸 수 있습니다.<br>핫토픽에는 글이 노출되지 않습니다.";
+        case "READWRITE":
+          return "모든 회원이 토픽을 구독할 수 있습니다.<br>구독하지 않아도 글을 볼 수 있습니다.<br>핫토픽에 글이 노출됩니다.";
       }
     }
   },
-  methods:{
-    reset(){
+  methods: {
+    reset() {
       this.boardId = null;
       this.boardName = null;
       this.boardDescription = null;
-      this.allGroupAuth = 'READONLY';
+      this.allGroupAuth = "READONLY";
       this.allowAnonymous = true;
       this.useCategory = false;
       this.allowedGroups = [];
@@ -117,39 +113,41 @@ export default {
           .get("/boardId", {params: {boardId: this.boardId}, headers: {silent: true}})
           .then(response => {
             this.boardIdErrors = [];
-            if(typeof event === 'function'){
+            if (typeof event === "function") {
               event.call(this);
             }
           })
           .catch(error => {
             this.boardIdErrors = [error && error.response && error.response.data.message];
-            if(typeof event === 'function'){
+            if (typeof event === "function") {
               event.call(this);
             }
           });
       }
     },
-    save(){
-      this.checkEmail(() => {
+    save() {
+      this.checkBoardId(() => {
         if (this.$refs.form.validate() && this.boardIdErrors.length === 0) {
           this.loading = true;
           this.$axios
             .post("/board", {
               boardId: this.boardId,
               boardName: this.boardName,
-              boardType: 'T',
+              boardType: "T",
               boardDescription: this.boardDescription,
               allGroupAuth: this.allGroupAuth,
               allowAnonymous: this.allowAnonymous,
-              allowedGroups:this.allowedGroups,
-              useCategory:this.useCategory
+              allowedGroups: this.allowedGroups,
+              useCategory: this.useCategory
             })
             .then(response => {
-              this.$emit('resetBoard');
-              this.$emit('closeDialog');
+              this.loading = false;
+              this.$emit("resetBoard");
+              this.$emit("closeDialog");
             })
             .catch(error => {
               console.log(error.response);
+              this.loading = false;
               this.$store.dispatch("showSnackbar", {text: error.response ? error.response.data.message : "토픽을 만들지 못했습니다.", color: "error"});
             });
         } else {
@@ -158,24 +156,42 @@ export default {
       });
     }
   },
-  created(){
+  created() {
     this.$axios
       .get("/group", {headers: {silent: true}})
       .then(response => {
-        this.groupItems = response.data.map(x => ({text: x.groupName, value: x.groupId}));
+        this.groupItems = response.data.sort((a, b) => a.groupType < b.groupType);
+        let previous = null;
+        let i = 0;
+        while (i < this.groupItems.length) {
+          if (previous !== this.groupItems[i].groupType) {
+            if (previous) {
+              previous = this.groupItems[i].groupType;
+              this.groupItems.splice(i, 0, {divider: true});
+              i++;
+            } else {
+              previous = this.groupItems[i].groupType;
+            }
+            this.groupItems.splice(i, 0, {header: groupName[previous]});
+            i++;
+          }
+          i++;
+        }
+        console.log(this.groupItems);
+        this.groupItems = this.groupItems.map(x => (x.groupName ? {text: x.groupName, value: x.groupId} : x));
       })
       .catch(error => {
         this.$store.dispatch("showSnackbar", {text: `그룹 목록을 가져오지 못했습니다.${error && error.response && error.response.data ? "[" + error.response.data.message + "]" : ""}`});
       });
   },
-  watch:{
-    boardId(){
-      if(this.boardIdErrors.length > 0){
+  watch: {
+    boardId() {
+      if (this.boardIdErrors.length > 0) {
         this.boardIdErrors = [];
       }
     }
   }
-}  
+};
 </script>
 <style>
 form .flex {
