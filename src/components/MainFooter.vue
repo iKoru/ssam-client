bc3d9f261b61490d6662140b54bba170412e4a61
koru <glassonyou@gmail.com>
2018-12-24 06:26:34 +0000

    maintain footer contents centered

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,17 +1,16 @@
 <template id="mainFooter">
   <v-footer height="auto" class="white pa-3 app--footer" absolute inset>
     <v-layout :column="$vuetify.breakpoint.smAndDown" :reverse="$vuetify.breakpoint.smAndDown">
-      <v-flex>
+      <v-flex class="copyright">
         <div class="text-xs-center text-md-left">
           <span class="caption">Copyright &copy; {{ new Date().getFullYear() }} Pedagy. All rights reserved.</span>
         </div>
       </v-flex>
-      <v-flex text-xs-center text-md-left>
+      <v-flex text-xs-center>
         <router-link to="/about" class="caption mr-1">Pedagy 소개</router-link>|
         <router-link to="/privacy" class="caption mx-1">개인정보 처리방침</router-link>|
         <router-link to="/contract" class="caption ml-1">이용약관</router-link>
       </v-flex>
-      <v-spacer></v-spacer>
     </v-layout>
   </v-footer>
 </template>
@@ -21,3 +20,10 @@ export default {
   name: "mainFooter"
 };
 </script>
+<style>
+@media(min-width:960px){
+  .copyright{
+    position:absolute;
+  }
+}
+</style>
