add621161be17544b80833df6a7d0df117160139
iKoru <glassonyou@gmail.com>
2018-12-27 00:21:32 +0900

    switch position of topic and lounge in myboard page

--- src/components/MyBoard.vue
+++ src/components/MyBoard.vue
@@ -3,7 +3,35 @@
     <v-card-title primary-title>
       <v-layout column>
         <v-layout row wrap>
-          <v-flex xs12 :sm6="lounges.length > 0" :sm9="lounges.length === 0" :lg5="lounges.length > 0" :lg8="lounges.length === 0" :class="{'pr-2':$vuetify.breakpoint.mdAndUp, 'ml-auto':true, 'mr-auto':lounges.length === 0}">
+          <v-flex xs12 sm6 lg5 v-if="lounges.length > 0" :class="{'mt-5':$vuetify.breakpoint.xsOnly, 'pr-2':$vuetify.breakpoint.mdAndUp, 'ml-auto':true}">
+            <v-layout row class="titleRow">
+              <span class="title">라운지</span>
+            </v-layout>
+            <span class="ellipsis subtitle">신고로 인해 쓰기/읽기가 제한된 라운지만 보여집니다.</span>
+            <v-divider class="my-3"/>
+            <v-list dense>
+              <v-list-tile v-for="lounge in lounges" :key="lounge.boardId">
+                <v-layout row>
+                  <v-flex class="ellipsis">
+                    <div class="vertical-center ellipsis">
+                      {{lounge.boardName}}
+                      <v-icon v-if="lounge.isOwner" title="라운지지기">person</v-icon>
+                    </div>
+                  </v-flex>
+                  <v-spacer/>
+                  <v-tooltip v-if="lounge.writeRestrictDate" bottom>
+                    <v-chip slot="activator" color="red" text-color="white" small>쓰기제한</v-chip>
+                    <span>{{$moment(lounge.writeRestrictDate, 'YYYYMMDD').format('YYYY년 M월 D일까지')}}</span>
+                  </v-tooltip>
+                  <v-tooltip v-if="lounge.readRestrictDate" bottom>
+                    <v-chip slot="activator" color="red" text-color="white" small>읽기제한</v-chip>
+                    <span>{{$moment(lounge.readRestrictDate, 'YYYYMMDD').format('YYYY년 M월 D일까지')}}</span>
+                  </v-tooltip>
+                </v-layout>
+              </v-list-tile>
+            </v-list>
+          </v-flex>
+          <v-flex xs12 :sm6="lounges.length > 0" :sm9="lounges.length === 0" :lg5="lounges.length > 0" :lg8="lounges.length === 0" :class="{'pl-2':$vuetify.breakpoint.mdAndUp, 'mr-auto':true, 'ml-auto':lounges.length === 0}">
             <v-layout row :class="{'titleRow':true, 'mb-2':lounges.length === 0}">
               <span :class="{'title':lounges.length > 0, 'headline':lounges.length===0}">토픽</span>
               <v-spacer/>
@@ -42,34 +70,6 @@
             <v-layout v-else>아직 구독하고 있는 토픽이 없네요.
               <br>새로운 토픽을 구독해보세요!
             </v-layout>
-          </v-flex>
-          <v-flex xs12 sm6 lg5 v-if="lounges.length > 0" :class="{'mt-5':$vuetify.breakpoint.xsOnly, 'pl-2':$vuetify.breakpoint.mdAndUp, 'mr-auto':true}">
-            <v-layout row class="titleRow">
-              <span class="title">라운지</span>
-            </v-layout>
-            <span class="ellipsis subtitle">신고로 인해 쓰기/읽기가 제한된 라운지만 보여집니다.</span>
-            <v-divider class="my-3"/>
-            <v-list dense>
-              <v-list-tile v-for="lounge in lounges" :key="lounge.boardId">
-                <v-layout row>
-                  <v-flex class="ellipsis">
-                    <div class="vertical-center ellipsis">
-                      {{lounge.boardName}}
-                      <v-icon v-if="lounge.isOwner" title="라운지지기">person</v-icon>
-                    </div>
-                  </v-flex>
-                  <v-spacer/>
-                  <v-tooltip v-if="lounge.writeRestrictDate" bottom>
-                    <v-chip slot="activator" color="red" text-color="white" small>쓰기제한</v-chip>
-                    <span>{{$moment(lounge.writeRestrictDate, 'YYYYMMDD').format('YYYY년 M월 D일까지')}}</span>
-                  </v-tooltip>
-                  <v-tooltip v-if="lounge.readRestrictDate" bottom>
-                    <v-chip slot="activator" color="red" text-color="white" small>읽기제한</v-chip>
-                    <span>{{$moment(lounge.readRestrictDate, 'YYYYMMDD').format('YYYY년 M월 D일까지')}}</span>
-                  </v-tooltip>
-                </v-layout>
-              </v-list-tile>
-            </v-list>
           </v-flex>
         </v-layout>
       </v-layout>
@@ -149,11 +149,11 @@ export default {
         this.$store.dispatch("showSnackbar", {text: "인증을 받은 회원만 토픽을 만들 수 있습니다.", color: "error"});
         return;
       }
-      document.body.style.position = 'fixed';
+      document.body.style.position = "fixed";
       this.dialog = true;
     },
     closeDialog() {
-      document.body.style.position = 'initial';
+      document.body.style.position = "initial";
       this.dialog = false;
     },
     resetBoard() {
--- src/components/board/BoardExtractor.vue
+++ src/components/board/BoardExtractor.vue
@@ -4,65 +4,59 @@
     <span>기간별 베스트</span>
     <v-flex xs12 class="py-2 mx-2">
       <v-btn-toggle v-model="period" id="boardExtractorBtnGroup" flat>
-        <v-btn flat value="daily">
-          오늘
-        </v-btn>
-        <v-btn flat value="weekly">
-          이번주
-        </v-btn>
-        <v-btn flat value="monthly">
-          이번달
-        </v-btn>
+        <v-btn flat value="daily">오늘</v-btn>
+        <v-btn flat value="weekly">이번주</v-btn>
+        <v-btn flat value="monthly">이번달</v-btn>
       </v-btn-toggle>
     </v-flex>
     <small-document-list :list="targets"></small-document-list>
-    
   </div>
 </template>
 <script>
-import SmallDocumentList from './SmallDocumentList'
+import SmallDocumentList from "./SmallDocumentList";
 export default {
-  name: 'BoardExtractor', 
-  components:{
+  name: "BoardExtractor",
+  components: {
     SmallDocumentList
   },
-  data () {
+  data() {
     return {
       targets: [],
-      period: 'daily',
+      period: "daily",
       items: null
-    }
+    };
   },
-  created(){
-    this.$axios.get('/best', {params:{boardType:'L'}})
-    .then(response => {
-      this.items = response.data;
-      this.targets = this.items.daily
-    })
-    .catch(error => {
-      console.log(error);
-      this.$store.dispatch('showSnackbar', {text:`베스트 게시물을 가져오는 데 오류가 발생했습니다.${error.response?'['+error.response.data.message+']': ''}`, color:'error'})
-    })
+  created() {
+    this.$axios
+      .get("/best", {params: {boardType: "L"}, headers: {silent: true}})
+      .then(response => {
+        this.items = response.data;
+        this.targets = this.items.daily;
+      })
+      .catch(error => {
+        console.log(error);
+        this.$store.dispatch("showSnackbar", {text: `베스트 게시물을 가져오는 데 오류가 발생했습니다.${error.response ? "[" + error.response.data.message + "]" : ""}`, color: "error"});
+      });
   },
-  watch:{
-    period(val){
-      switch(val){
-        case 'daily':
-          this.targets = this.items.daily
+  watch: {
+    period(val) {
+      switch (val) {
+        case "daily":
+          this.targets = this.items.daily;
           break;
-        case 'weekly':
-          this.targets = this.items.weekly
+        case "weekly":
+          this.targets = this.items.weekly;
           break;
-        case 'monthly':
-          this.targets = this.items.monthly
+        case "monthly":
+          this.targets = this.items.monthly;
           break;
       }
     }
   }
-}
+};
 </script>
 <style>
-  #boardExtractorBtnGroup{
-    width:100%;
-  }
+#boardExtractorBtnGroup {
+  width: 100%;
+}
 </style>
