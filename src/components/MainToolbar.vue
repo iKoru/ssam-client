8648d534eb978782d0d14ddf589eb20918cda95e
koru <glassonyou@gmail.com>
2019-01-07 00:35:34 +0000

    align search icon centered

--- src/components/MainToolbar.vue
+++ src/components/MainToolbar.vue
@@ -3,7 +3,7 @@
     <v-toolbar-side-icon @click.stop="$store.dispatch('toggleMenuDrawer')" v-if="$vuetify.breakpoint.xsOnly"></v-toolbar-side-icon>
     <v-toolbar-title class="ml-0 pl-3 cursor-pointer" @click="goMain" title="pedagy 메인">Pedagy</v-toolbar-title>
     <v-spacer></v-spacer>
-    <router-link to="/search">
+    <router-link to="/search" class="d-flex fill-height">
       <v-icon>search</v-icon>
     </router-link>
     <v-menu offset-y right nudge-bottom="5px" :open-on-hover="$vuetify.breakpoint.smAndUp" v-model="menu">
