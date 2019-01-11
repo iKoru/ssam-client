80ea6f85de85f5310207e488c1d1e560c95c4a81
koru <glassonyou@gmail.com>
2019-01-11 06:55:15 +0000

    adjust error page for mobile device

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -7,21 +7,21 @@
             <div class="mr-3 hidden-sm-and-down ml-auto">
               <img :src="require('@/static/img/error/'+errorCode+'.svg')" alt="에러 이미지">
             </div>
-            <div :class="{'text-md-center':true, 'ml-auto':$vuetify.breakpoint.smAndDown, 'mr-auto':true}">
+            <div :class="{'text-xs-center':true, 'ml-auto':$vuetify.breakpoint.smAndDown, 'mr-auto':true}">
               <h1 class="errorCode">{{errorCode}}</h1>
               <h2 class="my-3 headline" v-if="errorCode === 400">올바르지 않은 요청입니다.</h2>
               <h2 class="my-3 headline" v-else-if="errorCode === 403">회원 인증 후 이용할 수 있습니다.</h2>
               <h2 class="my-3 headline" v-else-if="errorCode === 404">페이지를 찾을 수 없습니다.</h2>
               <h2 class="my-3 headline" v-else>요청을 처리하던 도중 에러가 발생하였습니다.</h2>
-              <div>
+              <v-layout :column="$vuetify.breakpoint.xsOnly">
                 <v-btn @click="$router.go(-1)">이전 페이지로 가기</v-btn>
                 <router-link to="/auth" v-if="errorCode === 403">
-                  <v-btn>인증 페이지로 가기</v-btn>
+                  <v-btn :block="$vuetify.breakpoint.xsOnly">인증 페이지로 가기</v-btn>
                 </router-link>
                 <router-link to="/">
-                  <v-btn color="primary">메인 페이지로 가기</v-btn>
+                  <v-btn :block="$vuetify.breakpoint.xsOnly" color="primary">메인 페이지로 가기</v-btn>
                 </router-link>
-              </div>
+              </v-layout>
             </div>
           </v-layout>
         </v-content>
