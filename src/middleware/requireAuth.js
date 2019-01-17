ec775d85242094e5fa7bd17b0abbe0b0d2175f77
iKoru <glassonyou@gmail.com>
2019-01-18 00:09:00 +0900

    remove console logging, adjust uppercase in name

--- src/middleware/requireAuth.js
+++ src/middleware/requireAuth.js
@@ -2,21 +2,17 @@ import store from '../store'
 import router from '../router'
 import checkSignin from './checkSignin'
 export default async (to, from, next) => {
-  console.log('middleware!!!')
   const result = await checkSignin(to, from, router.app, store);
   next()
   if (typeof result === 'boolean' && result) {
     if (store.getters.profile.auth === 'AUTHORIZED') {
       next();
     } else {
-      console.log(store.getters.profile.auth, '튕김')
       next();
     }
   } else if (typeof result === 'string') {
-    console.log(to, 'aaaa');
     next(result);
   } else {
-    console.log(result);
     next(result);
   }
 }
