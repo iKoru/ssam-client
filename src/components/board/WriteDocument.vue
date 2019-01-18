187e28e5eb7f8d7775201c0deb1abe06b91e8b81
iKoru <glassonyou@gmail.com>
2019-01-19 01:25:52 +0900

    add side column on write document page

--- src/components/board/WriteDocument.vue
+++ src/components/board/WriteDocument.vue
@@ -1,20 +1,20 @@
 <template>
-    <v-layout>
-      <v-flex row>
-        <Editor/>
-      </v-flex>
-    </v-layout>
+  <v-layout>
+    <v-flex row>
+      <Editor/>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  name: 'WriteDocument',
+  name: "WriteDocument",
   components: {
-    Editor: () => import('@/components/board/editor/Editor')
+    Editor: () => import("@/components/board/editor/Editor")
   },
-  created () {
-    console.log('write document')
-    this.$store.dispatch('setColumnType', 'HIDE_ALWAYS')
+  created() {
+    console.log("write document");
+    this.$store.dispatch("setColumnType", "HIDE_SM");
   }
-}
+};
 </script>
