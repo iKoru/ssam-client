64f045ca4104b866b44e5f4f3eccc229d421897b
koru <glassonyou@gmail.com>
2019-01-20 15:31:37 +0000

    add search documents on child board, entire open board functionalities

--- src/components/SearchDocument.vue
+++ src/components/SearchDocument.vue
@@ -18,18 +18,28 @@
             </div>
           </v-flex>
           <v-flex xs12 sm11 md10 lg9 class="mx-auto">
-            <v-data-table :headers="headers" xs12 :items="documents" id="documentTable" :rows-per-page-items="[10]" :loading="loading" :total-items="totalDocuments" :pagination.sync="pagination" :class="{'noResult':totalDocuments === 0}">
+            <v-data-table :headers="headers" xs12 :items="documents" id="searchDocumentTable" :rows-per-page-items="[10]" :loading="loading" :total-items="totalDocuments" :pagination.sync="pagination" hide-actions>
+              <template slot="headers" slot-scope="props">
+                <tr>
+                  <th v-for="header in props.headers" :key="header.value" :class="{'px-1':true, 'text-xs-center':header.align === 'center', 'text-xs-left':header.align === 'left', 'text-xs-right':header.align === 'right', 'font-weight-bold':true, 'black--text':true}" :width="header.width || false">{{header.text}}</th>
+                </tr>
+              </template>
               <template slot="items" slot-scope="props">
                 <tr @click="$router.push('/'+props.item.boardId + '/'+props.item.documentId)">
-                  <td class="text-xs-left" v-if="$vuetify.breakpoint.smAndUp">{{ boardItems.some(x=>x.value === props.item.boardId)?boardItems.find(x=>x.value === props.item.boardId).text:'(삭제된 게시판)' }}</td>
-                  <td class="text-xs-left multi-row cursor-pointer" @click.stop="openLink(`/${props.item.boardId}/${props.item.documentId}`)">
+                  <td class="pa-1 grey--text lighten-1">
+                    <!--prettyhtml-ignore-->
+                    <v-layout row justify-center>
+                      [{{ boards.some(x=>x.boardId === props.item.boardId)?($vuetify.breakpoint.smAndUp?boards.find(x=>x.boardId === props.item.boardId).boardName:boards.find(x=>x.boardId === props.item.boardId).boardName.replace(/\s/g, '').substring(0,2)):'' }}]
+                    </v-layout>
+                  </td>
+                  <td class="text-xs-left pa-1 multi-row cursor-pointer" @click.stop="openLink(`/${props.item.boardId}/${props.item.documentId}`)">
                     <a :href="`/${props.item.boardId}/${props.item.documentId}`" target="_blank">
                       {{props.item.title}}
                       <span class="primary--text" title="댓글 수">{{props.item.commentCount > 0?'['+props.item.commentCount+']':''}}</span>
                     </a>
                   </td>
-                  <td class="text-xs-right">{{ props.item.voteUpCount }}</td>
-                  <td class="text-xs-right">{{ $moment(props.item.writeDateTime, 'YYYYMMDDHHmmss').format('Y-MM-DD') }}</td>
+                  <td class="text-xs-right pa-1">{{ props.item.voteUpCount }}</td>
+                  <td class="text-xs-right pa-1">{{ $moment(props.item.writeDateTime, 'YYYYMMDDHHmmss').isSame($moment(), 'day')?$moment(props.item.writeDateTime, 'YYYYMMDDHHmmss').format('HH:mm'):$moment(props.item.writeDateTime, 'YYYYMMDDHHmmss').format('M/D') }}</td>
                 </tr>
               </template>
               <template slot="footer">
@@ -39,6 +49,9 @@
                 </td>
               </template>
             </v-data-table>
+          </v-flex>
+          <v-flex text-xs-center mt-2 xs12>
+            <v-pagination id="searchDocumentPagination" v-model="pagination.page" :length="pages" :total-visible="$vuetify.breakpoint.smAndUp?10:undefined"></v-pagination>
           </v-flex>
         </v-card-title>
       </v-card>
@@ -69,22 +82,29 @@ export default {
     };
   },
   computed: {
+    boards(){
+      return this.$store.getters.boards;
+    },
     boardItems() {
-      return this.$store.getters.boards.filter(x=>x.boardType !== 'T').concat(this.$store.getters.userBoards.filter(x=>x.boardType === 'T')).map(x=>({text:x.boardName, value:x.boardId}));
+      const auth = this.$store.getters.profile.auth
+      const userBoards = this.$store.getters.userBoards;
+      let boardItems = this.boards.filter(x=>(x.boardType !== 'T' && x.statusAuth.read.includes(auth)) || (x.boardType === 'T' && userBoards.some(y=>y.boardId === x.boardId && y.boardType === 'T'))).map(x=>({text:x.boardName, value:x.boardId}));
+      boardItems.splice(0, 0, {text:'(공개 게시판 전체)', value:null})
+      return boardItems
     },
     noresult(){
       return this.targetYear + '년을 대상으로 검색한 결과입니다.' + (this.targetYear > 2018? ' 이전 연도로 계속 검색할 수 있습니다.' : '')
     },
     headers() {
-      return this.$vuetify.breakpoint.xsOnly ? [{text: "제목", sortable: false, align: "left", value: "title", class: "ellipsis", width: "100%"}, {text: "추천", align: "right", sortable: false, value: "voteUpCount"}, {text: "작성일", sortable: false, align: "right", value: "writeDateTime"}] : [{text: "게시판", align: "left", sortable: false, value: "boardId"}, {text: "제목", sortable: false, align: "left", value: "title", class: "ellipsis", width: "100%"}, {text: "추천", align: "right", sortable: false, value: "voteUpCount"}, {text: "작성일", sortable: false, align: "right", value: "writeDateTime"}];
+      return [{text: "게시판", align: "center", sortable: false, value: "boardId", width: this.$vuetify.breakpoint.smAndUp ? "100" : "50"}, {text: "제목", sortable: false, align: "center", value: "title", class: "ellipsis", width: "100%"}, {text: "추천", align: "right", sortable: false, value: "voteUpCount", width: "30"}, {text: "날짜", sortable: false, align: "right", value: "writeDateTime", width: this.$vuetify.breakpoint.xsOnly ? "50" : "100"}];
+    },
+    pages() {
+      return this.pagination.rowsPerPage ? Math.ceil(this.totalDocuments / this.pagination.rowsPerPage) : 0;
     }
   },
   methods: {
     search(){
-      if(!this.boardId){
-        this.$store.dispatch('showSnackbar', {text:'검색할 게시판을 선택해주세요.',color:'error'});
-        return;
-      }else if(!this.searchQuery){
+      if(!this.searchQuery){
         this.$store.dispatch('showSnackbar', {text:'검색할 단어를 입력해주세요.',color:'error'});
         return;
       }
@@ -93,10 +113,7 @@ export default {
       this.searched = true;
     },
     searchMore(){
-      if(!this.boardId){
-        this.$store.dispatch('showSnackbar', {text:'검색할 게시판을 선택해주세요.',color:'error'});
-        return;
-      }else if(!this.searchQuery){
+      if(!this.searchQuery){
         this.$store.dispatch('showSnackbar', {text:'검색할 단어를 입력해주세요.',color:'error'});
         return;
       }
@@ -128,14 +145,21 @@ export default {
 };
 </script>
 <style>
-table.v-table thead td:not(:nth-child(1)),
-table.v-table tbody td:not(:nth-child(1)),
-table.v-table thead th:not(:nth-child(1)),
-table.v-table tbody th:not(:nth-child(1)),
-table.v-table thead td:first-child,
-table.v-table tbody td:first-child,
-table.v-table thead th:first-child,
-table.v-table tbody th:first-child {
-  padding: 0 12px;
+#searchDocumentTable tbody tr:last-child {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+}
+#searchDocumentPagination .v-pagination__item,
+#searchDocumentPagination .v-pagination__item--active,
+#searchDocumentPagination .v-pagination__navigation {
+  box-shadow: none;
+  margin: 0;
+  padding: 0;
+}
+#searchDocumentPagination .v-pagination__item--active {
+  color: black;
+  font-weight: bold;
+  font-size: 16px;
+  background-color: white !important;
+  border-color: white !important;
 }
 </style>
