cae307f317c7235208d512420ac3750598be0995
koru <glassonyou@gmail.com>
2019-02-12 08:31:58 +0000

    add card view functionalities

--- src/components/board/DocumentList.vue
+++ src/components/board/DocumentList.vue
@@ -2,7 +2,49 @@
   <v-flex>
     <v-layout column>
       <v-flex>
-        <v-data-table :headers="headers" xs12 :items="documents" class="last-tr-border" id="documentTable" hide-actions :rows-per-page-items="[$vuetify.breakpoint.xsOnly?10:20]" :loading="loading" :total-items="totalDocuments" :pagination.sync="pagination" :no-data-text="noDataText">
+        <template v-if="isCardView">
+          <v-divider/>
+          <v-list class="py-0 border-x-light" dense>
+            <template v-for="document in documents.filter(x=>x.isNotice)">
+              <v-list-tile :to="`/${board.boardId}/${document.documentId}`" :key="document.documentId" class="grey lighten-3">
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    <div class="ellipsis text-xs-left body-1 font-weight-bold">
+                      <router-link :to="`/${board.boardId}/${document.documentId}`" @click.native.stop>[공지] {{document.title}}</router-link>
+                    </div>
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider :key="'divider'+document.documentId"/>
+            </template>
+          </v-list>
+          <v-list two-line class="py-0 border-x-light" id="cardView">
+            <template v-for="document in documents.filter(x=>!x.isNotice)">
+              <v-list-tile :to="`/${board.boardId}/${document.documentId}`" :key="document.documentId" class="grey lighten-5">
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    <v-layout row>
+                      <!--prettyhtml-ignore-->
+                      <small v-if="hasChildren" class="grey--text lighten-1 mr-1">
+                        [<router-link :to="'/'+document.boardId" class="grey--text lighten-1" @click.native.stop>{{ boardItems.some(x=>x.boardId === document.boardId)?($vuetify.breakpoint.smAndUp?boardItems.find(x=>x.boardId === document.boardId).boardName.replace(/\s/g, '').substring(0,5):boardItems.find(x=>x.boardId === document.boardId).boardName.replace(boardTypeItems[board.boardType],'').replace(/\s/g, '').substring(0,2)):'' }}</router-link>]
+                      </small>
+                      <div class="ellipsis font-weight-bold body-2">
+                        {{document.title}}
+                      </div>
+                      <v-spacer/>
+                      <div class="caption">
+                        {{document.nickName}} | <v-icon color="primary" small>thumb_up_alt</v-icon> <span class="primary--text">{{document.voteUpCount}}</span> | <v-icon color="accent" small>chat_bubble_outline</v-icon> <span class="accent--text">{{document.commentCount}}</span> | <span class="grey--text lighten-1">{{ $moment(document.writeDateTime, 'YYYYMMDDHHmmss').isSame($moment(), 'day')?$moment(document.writeDateTime, 'YYYYMMDDHHmmss').format('HH:mm'):$moment(document.writeDateTime, 'YYYYMMDDHHmmss').format($vuetify.breakpoint.xsOnly?'M/D':'Y/M/D') }}</span>
+                      </div>
+                    </v-layout>
+                  </v-list-tile-title>
+                  <v-list-tile-sub-title>{{document.previewContents || '(텍스트가 없는 글입니다.)'}}</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider :key="'divider'+document.documentId"/>
+            </template>
+          </v-list>
+        </template>
+        <v-data-table v-else :headers="headers" xs12 :items="documents" class="last-tr-border" id="documentTable" hide-actions :rows-per-page-items="[$vuetify.breakpoint.xsOnly?10:20]" :loading="loading" :total-items="totalDocuments" :pagination.sync="pagination" :no-data-text="noDataText">
           <template slot="headers" slot-scope="props">
             <tr>
               <th v-for="header in props.headers" :key="header.value" :class="{'px-1 font-weight-bold black--text body-2':true, 'text-xs-center':header.align === 'center', 'text-xs-left':header.align === 'left', 'text-xs-right':header.align === 'right'}" :width="header.width || false">{{header.text}}</th>
@@ -48,7 +90,8 @@
       </v-flex>
       <v-flex>
         <v-layout row pa-2 align-center>
-          <v-btn flat small @click="getDocuments" class="grey--text short" color="secondary">새로고침</v-btn>
+          <v-btn flat small @click="switchView" icon :title="isCardView?'목록형 보기':'카드형 보기'"><v-icon>{{isCardView?'list':'dashboard'}}</v-icon></v-btn>
+          <v-btn flat small @click="getDocuments" class="grey--text short" color="secondary" :loading="loading">새로고침</v-btn>
           <v-spacer/>
           <v-flex xs6 sm4 id="searchDocumentForm">
             <v-text-field hide-details dense class="dense mt-0 pt-0" v-model="searchQuery" append-outer-icon="search" @keydown.enter.stop="search" @click:append-outer="search" placeholder="제목 또는 내용으로 검색"></v-text-field>
@@ -77,12 +120,13 @@ export default {
       page: null, // page element for pagination component
       boardId: null, // local current boardId,
       noDataText: '아직 작성된 글이 없습니다. 첫 글을 작성해보세요!',
-      searchQuery: null
+      searchQuery: null,
+      isCardView: !!localStorage.getItem('CardView')
     };
   },
   computed: {
     headers () {
-      let headers = [{ text: '제목', value: 'title', sortable: false, align: 'center' }, { text: '추천', value: 'voteUpCount', sortable: false, align: 'right', width: this.$vuetify.breakpoint.xsOnly ? '30' : '50' }, { text: '날짜', value: 'writeDateTime', sortable: false, align: 'right', width: this.$vuetify.breakpoint.xsOnly ? '50' : '100' }];
+      let headers = [{ text: '제목', value: 'title', sortable: false, align: 'center' }, { text: '추천', value: 'voteUpCount', sortable: false, align: 'right', width: this.$vuetify.breakpoint.xsOnly ? '30' : '50' }, { text: '날짜', value: 'writeDateTime', sortable: false, align: 'center', width: this.$vuetify.breakpoint.xsOnly ? '50' : '100' }];
       if (this.$vuetify.breakpoint.smAndUp) {
         headers.splice(1, 0, { text: '글쓴이', value: 'nickName', sortable: false, align: 'center', width: '100' });
       }
@@ -130,6 +174,10 @@ export default {
       if (this.searchQuery) {
         this.$router.push(`/searchDocument?boardId=${this.board.boardId}&searchQuery=${this.searchQuery}`);
       }
+    },
+    switchView () {
+      this.isCardView = !this.isCardView;
+      localStorage.setItem('CardView', this.isCardView || null);
     }
   },
   created () {
@@ -179,5 +227,9 @@ export default {
 }
 #searchDocumentForm {
   min-width: 200px;
+}
+#cardView .v-list__tile__content .v-icon{
+  vertical-align:text-top;
+  margin-right:4px;
 }
 </style>
