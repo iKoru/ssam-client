995624176cbbcf8cd713e158a3c3e0240f55020f
koru <glassonyou@gmail.com>
2019-02-14 02:32:47 +0000

    build v2019.02.14

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,12 +1,12 @@
-importScripts("/precache-manifest.3794e4388253fd7020b89024054bda33.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.e1086fec12fc038057385bf45482741b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global workbox */
 if (workbox) {
-  console.log(`Workbox is loaded`);
+    console.log(`Workbox is loaded`);
 
-  workbox.precaching.precacheAndRoute(self.__precacheManifest);
+    workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-}
+} 
 else {
-  console.log(`Workbox didn't load`);
+    console.log(`Workbox didn't load`);
 }
