ed95488d3b4c7d95b7b088da183784826da9632c
iKoru <glassonyou@gmail.com>
2019-02-15 00:46:26 +0900

    build v2019.02.15

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,12 +1,12 @@
-importScripts("/precache-manifest.e1086fec12fc038057385bf45482741b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.3493381cb5713b164ca03e745fa88784.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global workbox */
 if (workbox) {
-    console.log(`Workbox is loaded`);
+  console.log(`Workbox is loaded`);
 
-    workbox.precaching.precacheAndRoute(self.__precacheManifest);
+  workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-} 
+}
 else {
-    console.log(`Workbox didn't load`);
+  console.log(`Workbox didn't load`);
 }
