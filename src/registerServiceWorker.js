f6c467f12086fe36d2e3acafc5d41af6c877d9d3
iKoru <glassonyou@gmail.com>
2019-02-16 22:47:28 +0900

    remove logs on service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,26 +1,24 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker'
-console.log('registering service worker...')
 if (process.env.NODE_ENV === 'production') {
-  console.log('production mode...')
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
+    ready() {
       console.log(
         'App is being served from cache by a service worker.\n' +
         'For more details, visit https://goo.gl/AFskqB'
       )
     },
-    cached () {
+    cached() {
       console.log('Content has been cached for offline use.')
     },
-    updated () {
+    updated() {
       console.log('New content is available; please refresh.')
     },
-    offline () {
+    offline() {
       console.log('No internet connection found. App is running in offline mode.')
     },
-    error (error) {
+    error(error) {
       console.error('Error during service worker registration:', error)
     }
   })
