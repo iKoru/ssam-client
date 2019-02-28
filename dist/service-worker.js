067f5094411012069ee97e5360ac5bd0110f876c
koru <glassonyou@gmail.com>
2019-02-28 04:20:17 +0000

    build v2019.02.28.4

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.b8ae010e1706df511ca364ef610aaf1a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.c214030992cde89df9529f3bbc8c4903.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global workbox */
 if (workbox) {
